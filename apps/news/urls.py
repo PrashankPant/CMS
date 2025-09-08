5924e0051c42ee2c6009dd8ff498c2b1f4d61b85
Prashank Pant <123922009+PrashankPant@users.noreply.github.com>
2025-09-08 15:14:52 +0545

    Update urls.py

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -1,9 +1,1 @@
-from .views import NewsViewSet
-from django.urls import include , path
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-
-router.register(r'news',NewsViewSet,basename="news")
-
-urlpatterns = router.urls
+from django.urls import include
